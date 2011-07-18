15c30309aa2c15daf16357df8fc78ffa627e6bdc
Mason Stewart <masondesu@gmail.com>
2011-07-17 23:13:17 -0400

    Initial commit. Whew.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainbowburger::Application.config.secret_token = '17a258ae35c1c8149ea60fc0881f24413850e515105fea1e2d0cb2484ba71053d563e8235ba1609e6c6a3dac5a40769eb0d18f41a77946b8da2339333021db70'
